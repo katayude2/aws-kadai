b8d1bed6200cc7160fcf51766697d7b9fd4bb698
daisuke_watanabe2 <daisuke_watanabe2@icloud.com>
2020-03-23 19:21:51 +0900

    sockファイルのパスを設定

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,8 +2,8 @@ $worker  = 2
 $timeout = 30
 #自分のアプリケーション名（currentがつくことに注意）
 $app_dir = "/var/www/achieve/current"
-$listen  = File.expand_path '/var/www/achieve/public/sockets/unicorn.sock', $app_dir
-$pid     = File.expand_path '/var/www/achieve/public/pids/unicorn.pid', $app_dir
+$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
+$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
 # 上記で設定したものが適応されるよう定義
 worker_processes  $worker
